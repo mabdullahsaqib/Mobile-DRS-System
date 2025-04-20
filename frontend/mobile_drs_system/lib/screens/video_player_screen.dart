2ecc0bfeb6ace259b83a9288d43b229187d7dd27
Arham Ali <125752248+MuahmmadArhamAli@users.noreply.github.com>
2025-04-20 18:10:11 +0500

    Deleting the video paths after receiving them in the video player screen

--- frontend/mobile_drs_system/lib/screens/video_player_screen.dart
+++ frontend/mobile_drs_system/lib/screens/video_player_screen.dart
@@ -1,6 +1,8 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import '../providers/video_save.dart';
+import 'package:provider/provider.dart';
 
-class VideoPlayerScreen extends StatelessWidget {
+class VideoPlayerScreen extends StatefulWidget {
   final String mainVideoPath;
   final String secondaryVideoPath;
 
@@ -11,10 +13,27 @@ class VideoPlayerScreen extends StatelessWidget {
   });
 
   @override
+  State<VideoPlayerScreen> createState() => _VideoPlayerScreenState();
+}
+
+class _VideoPlayerScreenState extends State<VideoPlayerScreen> {
+  late VideoSaveDataProvider _videoSaveDataProvider;
+
+  @override
+  void initState() {
+    super.initState();
+    _videoSaveDataProvider =
+        Provider.of<VideoSaveDataProvider>(context, listen: false);
+    _videoSaveDataProvider.clearMainVideoPath();
+    _videoSaveDataProvider.clearSecondaryVideoPath();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Container(
       child: Center(
-        child: Text('Video Player Screen'),
+        child:
+            Text('Video Player Screen'), // Placeholder for video player widget
       ),
     );
   }
