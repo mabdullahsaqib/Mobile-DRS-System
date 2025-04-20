b1d548cdbebb24d5788845a607b35e7f56c17bd4
Arham Ali <125752248+MuahmmadArhamAli@users.noreply.github.com>
2025-04-20 18:27:02 +0500

    Showing videos in the video player screen

--- frontend/mobile_drs_system/lib/screens/video_player_screen.dart
+++ frontend/mobile_drs_system/lib/screens/video_player_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import '../providers/video_save.dart';
+import 'package:video_player/video_player.dart';
 import 'package:provider/provider.dart';
+import 'dart:io';
 
 class VideoPlayerScreen extends StatefulWidget {
   final String mainVideoPath;
@@ -19,6 +21,9 @@ class VideoPlayerScreen extends StatefulWidget {
 class _VideoPlayerScreenState extends State<VideoPlayerScreen> {
   late VideoSaveDataProvider _videoSaveDataProvider;
 
+  late VideoPlayerController _mainVideoController;
+  late VideoPlayerController _secondaryVideoController;
+
   @override
   void initState() {
     super.initState();
@@ -26,14 +31,49 @@ class _VideoPlayerScreenState extends State<VideoPlayerScreen> {
         Provider.of<VideoSaveDataProvider>(context, listen: false);
     _videoSaveDataProvider.clearMainVideoPath();
     _videoSaveDataProvider.clearSecondaryVideoPath();
+
+    _mainVideoController = VideoPlayerController.file(
+      File(widget.mainVideoPath),
+    )..initialize().then((_) {
+        setState(() {});
+      });
+
+    _secondaryVideoController = VideoPlayerController.file(
+      File(widget.secondaryVideoPath),
+    )..initialize().then((_) {
+        setState(() {});
+      });
+  }
+
+  @override
+  void dispose() {
+    _mainVideoController.dispose();
+    _secondaryVideoController.dispose();
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Center(
-        child:
-            Text('Video Player Screen'), // Placeholder for video player widget
+    return Scaffold(
+      appBar: AppBar(title: Text('Video Player')),
+      body: Column(
+        children: [
+          if (_mainVideoController.value.isInitialized)
+            AspectRatio(
+              aspectRatio: _mainVideoController.value.aspectRatio,
+              child: VideoPlayer(_mainVideoController),
+            )
+          else
+            CircularProgressIndicator(),
+          const SizedBox(height: 20),
+          if (_secondaryVideoController.value.isInitialized)
+            AspectRatio(
+              aspectRatio: _secondaryVideoController.value.aspectRatio,
+              child: VideoPlayer(_secondaryVideoController),
+            )
+          else
+            CircularProgressIndicator(),
+        ],
       ),
     );
   }
