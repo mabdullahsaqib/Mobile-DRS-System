226b1be805d6c1529cceb9ca28a72f971695b2b8
Ahmad-fraz-4101 <l226735@lhr.nu.edu.pk>
2025-04-21 18:24:58 +0500

    Delete unnecessary files

--- backend/app/modules/trajectory_analysis/controllers/frame_controller.py
+++
@@ -1,6 +0,0 @@
-from modules.trajectory_analysis.models.frame_models import TrajectoryData
-
-
-def process_frame(frame: TrajectoryData):
-    print(f"Processing frame {frame.frame_id}")
-    return {"message": "Frame processed"}
