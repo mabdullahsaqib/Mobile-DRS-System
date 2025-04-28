b5af6bef204096eb8b25483cc9dfca3c17917012
Muhammad Bilal <muhammad.bilal832017@gmail.com>
2025-04-29 00:23:28 +0500

    mainActivity works, needs threading implementation

--- frontend/mobile_drs_system/android/app/src/main/kotlin/com/example/mobile_drs_system/MainActivity.kt
+++ frontend/mobile_drs_system/android/app/src/main/kotlin/com/example/mobile_drs_system/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.flutter_method_channel
+package com.example.mobile_drs_system
 
 import android.graphics.Bitmap
 import android.media.MediaExtractor
@@ -14,7 +14,7 @@ import java.io.File
 import java.io.FileOutputStream
 
 class MainActivity : FlutterActivity() {
-    private val CHANNEL = "com.example/video_frames_extractor"
+    private val CHANNEL = "com.example.video_frames_extractor"
 
     override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
         super.configureFlutterEngine(flutterEngine)
