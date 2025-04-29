4a5009b4bc5cfeb8e0bcbf37bec8806d7daefbb7
Arham Ali <125752248+MuahmmadArhamAli@users.noreply.github.com>
2025-04-29 21:11:55 +0500

    Video aspect ratio is fixed, added buttons on top of the video and showing timestamp as well, Video is working with both landscape and portrait

--- frontend/mobile_drs_system/lib/screens/video_player_screen.dart
+++ frontend/mobile_drs_system/lib/screens/video_player_screen.dart
@@ -17,6 +17,13 @@ class VideoPlayerScreen extends StatefulWidget {
 class _VideoPlayerScreenState extends State<VideoPlayerScreen> {
   late VideoPlayerController _controller;
 
+  String formatVideoDuration(Duration duration) {
+    String twoDigits(int n) => n.toString().padLeft(2, '0');
+    String twoDigitMinutes = twoDigits(duration.inMinutes.remainder(60));
+    String twoDigitSeconds = twoDigits(duration.inSeconds.remainder(60));
+    return "$twoDigitMinutes:$twoDigitSeconds";
+  }
+
   @override
   void initState() {
     super.initState();
@@ -28,53 +35,103 @@ class _VideoPlayerScreenState extends State<VideoPlayerScreen> {
 
   @override
   Widget build(BuildContext context) {
+    final isLandscape =
+        MediaQuery.of(context).orientation == Orientation.landscape;
     return Scaffold(
       appBar: AppBar(title: const Text('Video Player')),
       body: Center(
         child: _controller.value.isInitialized
-            ? Column(
+            ? Stack(
                 children: [
-                  AspectRatio(
-                    aspectRatio: _controller.value.aspectRatio,
-                    child: VideoPlayer(_controller),
-                  ),
-                  VideoProgressIndicator(
-                    _controller,
-                    allowScrubbing: true,
-                  ),
-                  Row(
-                    children: [
-                      IconButton(
-                          onPressed: () {
-                            _controller.seekTo(Duration(
-                                seconds:
-                                    _controller.value.position.inSeconds - 1));
-                          },
-                          icon: const Icon(Icons.arrow_back)),
-                      const SizedBox(width: 10),
-                      IconButton(
-                        icon: Icon(
-                          _controller.value.isPlaying
-                              ? Icons.pause
-                              : Icons.play_arrow,
+                  isLandscape
+                      ? SizedBox.expand(
+                          child: FittedBox(
+                            fit: BoxFit.cover,
+                            child: SizedBox(
+                              width: _controller.value.size.width,
+                              height: _controller.value.size.height,
+                              child: VideoPlayer(_controller),
+                            ),
+                          ),
+                        )
+                      : SizedBox.expand(
+                          child: FittedBox(
+                            fit: BoxFit.contain, // Or BoxFit.contain
+                            child: SizedBox(
+                              width: _controller.value.size.height,
+                              height: _controller.value.size.width,
+                              child: VideoPlayer(_controller),
+                            ),
+                          ),
                         ),
-                        onPressed: () {
-                          setState(() {
-                            _controller.value.isPlaying
-                                ? _controller.pause()
-                                : _controller.play();
-                          });
-                        },
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Row(
+                        children: [
+                          const SizedBox(width: 20),
+                          Text(
+                              textAlign: TextAlign.left,
+                              "${formatVideoDuration(_controller.value.position)} / ${formatVideoDuration(_controller.value.duration)}",
+                              style: const TextStyle(
+                                color: Colors.white,
+                                fontSize: 16,
+                              )),
+                        ],
                       ),
-                      const SizedBox(width: 10),
-                      IconButton(
-                        onPressed: () {
-                          _controller.seekTo(Duration(
-                              seconds:
-                                  _controller.value.position.inSeconds + 1));
-                        },
-                        icon: const Icon(Icons.arrow_forward),
-                      )
+                      SizedBox(
+                        height: 15,
+                        child: VideoProgressIndicator(
+                          _controller,
+                          allowScrubbing: true,
+                        ),
+                      ),
+                      const SizedBox(height: 10),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          IconButton(
+                            onPressed: () {
+                              _controller.seekTo(Duration(
+                                  seconds:
+                                      _controller.value.position.inSeconds -
+                                          1));
+                            },
+                            icon: const Icon(Icons.arrow_back),
+                            color: Colors.white,
+                          ),
+                          const SizedBox(width: 10),
+                          IconButton(
+                            icon: Icon(
+                              _controller.value.isPlaying
+                                  ? Icons.pause
+                                  : Icons.play_arrow,
+                              color: Colors.white,
+                            ),
+                            onPressed: () {
+                              setState(() {
+                                _controller.value.isPlaying
+                                    ? _controller.pause()
+                                    : _controller.play();
+                              });
+                            },
+                          ),
+                          const SizedBox(width: 10),
+                          IconButton(
+                            onPressed: () {
+                              _controller.seekTo(Duration(
+                                  seconds:
+                                      _controller.value.position.inSeconds +
+                                          1));
+                            },
+                            icon: const Icon(Icons.arrow_forward),
+                            color: Colors.white,
+                          )
+                        ],
+                      ),
+                      const SizedBox(height: 30),
                     ],
                   ),
                 ],
