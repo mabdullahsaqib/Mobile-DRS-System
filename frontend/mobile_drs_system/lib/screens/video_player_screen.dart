06269dccce57392df64f8edcf7385af20601a706
Arham Ali <125752248+MuahmmadArhamAli@users.noreply.github.com>
2025-04-29 19:42:12 +0500

    Added Video Player with pause, play and skip to a slide value and forward and backward one second

--- frontend/mobile_drs_system/lib/screens/video_player_screen.dart
+++ frontend/mobile_drs_system/lib/screens/video_player_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:video_player/video_player.dart';
+import 'dart:io';
 
-class VideoPlayerScreen extends StatelessWidget {
+class VideoPlayerScreen extends StatefulWidget {
   final String mainVideoPath;
 
   const VideoPlayerScreen({
@@ -9,16 +11,75 @@ class VideoPlayerScreen extends StatelessWidget {
   });
 
   @override
+  State<VideoPlayerScreen> createState() => _VideoPlayerScreenState();
+}
+
+class _VideoPlayerScreenState extends State<VideoPlayerScreen> {
+  late VideoPlayerController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = VideoPlayerController.file(File(widget.mainVideoPath))
+      ..initialize().then((_) {
+        setState(() {});
+      });
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Video Player')),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text('Main Video Path: $mainVideoPath'),
-          ],
-        ),
+        child: _controller.value.isInitialized
+            ? Column(
+                children: [
+                  AspectRatio(
+                    aspectRatio: _controller.value.aspectRatio,
+                    child: VideoPlayer(_controller),
+                  ),
+                  VideoProgressIndicator(
+                    _controller,
+                    allowScrubbing: true,
+                  ),
+                  Row(
+                    children: [
+                      IconButton(
+                          onPressed: () {
+                            _controller.seekTo(Duration(
+                                seconds:
+                                    _controller.value.position.inSeconds - 1));
+                          },
+                          icon: const Icon(Icons.arrow_back)),
+                      const SizedBox(width: 10),
+                      IconButton(
+                        icon: Icon(
+                          _controller.value.isPlaying
+                              ? Icons.pause
+                              : Icons.play_arrow,
+                        ),
+                        onPressed: () {
+                          setState(() {
+                            _controller.value.isPlaying
+                                ? _controller.pause()
+                                : _controller.play();
+                          });
+                        },
+                      ),
+                      const SizedBox(width: 10),
+                      IconButton(
+                        onPressed: () {
+                          _controller.seekTo(Duration(
+                              seconds:
+                                  _controller.value.position.inSeconds + 1));
+                        },
+                        icon: const Icon(Icons.arrow_forward),
+                      )
+                    ],
+                  ),
+                ],
+              )
+            : const CircularProgressIndicator(),
       ),
     );
   }
